734186457b3ac04c742a5108faaf8f0fbfa77637
becky giffen <rebecca.giffen@students.plymouth.ac.uk>
2021-03-03 20:44:45 +0000

    debugged select menu - previoisly wasnt working. added fixed navbar

--- scripts/index.js
+++ scripts/index.js
@@ -49,12 +49,6 @@ document.addEventListener('DOMContentLoaded', function() {
  
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('select').formSelect();
+    var instances = M.FormSelect.init(elems, {});
   });
         
