96dc5afcea57980599a84834649188efbfe83316
becky giffen <rebecca.giffen@students.plymouth.ac.uk>
2021-03-03 17:36:25 +0000

    added materialise properties, created drop down which needs fixing

--- scripts/index.js
+++ scripts/index.js
@@ -46,3 +46,15 @@ document.addEventListener('DOMContentLoaded', function() {
   
   
   });
+ 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+        
